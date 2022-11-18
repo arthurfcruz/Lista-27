aff14a5e9148917e67ae83e5d3df1a8625e91d87
Arthur <arthurfelixcruz@gmail.com>
2022-11-17 22:01:22 -0300

    criando as classes produto e venda com suas funcionalidades

--- teste.js
+++ teste.js
@@ -23,3 +23,30 @@ class Vendedor extends Funcionario{
         console.log(this.Nome, this.Salario, this.calcularSalario(), this.PercentualComissao,"%")    
     }
 }
+
+class Produto{
+    Nome
+    Valor
+}
+
+class Venda{
+    Vendedor
+    ListaProdutos = []
+    ValorTotal
+
+    AdicionarProduto() {
+        let produto = new Produto()
+        this.ListaProdutos.push(produto)
+    }
+
+    CalcularTotal() {
+        this.ValorTotal = 0
+        for (let index = 0; index < this.ListaProdutos.length; index++) {
+            this.ValorTotal = this.ValorTotal + this.ListaProdutos[index].Valor
+        } 
+    }
+
+    FinalizarVenda() {
+        console.log(this.Vendedor.Nome, this.ValorTotal)
+    }
+}
