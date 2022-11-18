6cd1331f83fec8d435161cf2af007af99e38c35b
Arthur <arthurfelixcruz@gmail.com>
2022-11-17 21:48:32 -0300

    dando funcionalidade as classes

--- teste.js
+++ teste.js
@@ -6,8 +6,20 @@ class Funcionario{
 class Gerente extends Funcionario{
     Departamento
 
+    exibirInformacoes(){
+        console.log(this.Nome, this.Salario, this.Departamento)
+    }
 }
 
 class Vendedor extends Funcionario{
     PercentualComissao
+
+    calcularSalario(){
+        let SalarioAumentado = this.Salario + (this.Salario * (this.PercentualComissao / 100))
+        return SalarioAumentado
+    }
+
+    ExibirInformacoes(){
+        console.log(this.Nome, this.Salario, this.calcularSalario(), this.PercentualComissao,"%")    
+    }
 }
